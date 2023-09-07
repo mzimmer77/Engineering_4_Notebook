ac9d11b50a8d817e77e5a4062b7b1249d9ddbd30
mzimmer77 <mzimmer77@charlottesvilleschools.org>
2023-09-07 10:43:42 -0400

    launchpadP1

---
+++ raspberry-pi/launchpadP1.py
@@ -0,0 +1,7 @@
+# type: ignore #ignores problems
+import time #adds time
+
+for x in range (10,0,-1): #using intervals that start at 10 and go to 0 with intervals every -1
+    print (x) #prints my numbers
+    time.sleep (1) #counts down every second
+print ("we have liftoff") #prints we have liftoff
