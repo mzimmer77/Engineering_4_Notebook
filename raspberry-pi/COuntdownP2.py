a8491fc3f33f1ba3a918f81be14037e4a6c13043
mzimmer77 <mzimmer77@charlottesvilleschools.org>
2023-09-08 10:49:17 -0400

    countdown p2V1

---
+++ raspberry-pi/COuntdownP2.py
@@ -0,0 +1,18 @@
+# type: ignore
+import time 
+import digitalio 
+import board
+
+ledGreen = digitalio.DigitalInOut(board.GP0)
+ledRed = digitalio.DigitalInOut(board.GP1)
+ledGreen.direction = digitalio.Direction.OUTPUT 
+ledRed.direction = digitalio.Direction.OUTPUT  
+
+for x in range (10,0,-1): #using intervals that start at 10 and go to 0 with intervals every -1
+    print (x) #prints my 
+    ledRed.value = True
+    time.sleep (0.5)
+    ledRed.value = False
+    time.sleep (0.5) #counts down every second
+print ("we have liftoff") #prints we have liftoff
+
